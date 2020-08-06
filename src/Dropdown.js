28f04ba661bd98f7ed04887a7fdfb00d7399e9b7
aksh0314 <akshay.sonawane343@gmail.com>
2020-08-06 16:31:45 +0530

    added filter search functionality

--- src/Dropdown.js
+++ src/Dropdown.js
@@ -1,19 +1,21 @@
 import React, { useState } from 'react'
 import './Repo.css'
 
-export default function Dropdown({options}) {
-
+export default function Dropdown({options, getSelection}) {
     const [val, setVal] = useState('Select an Option')
     
     const handleChange = (e) => {
         setVal(e.target.value)
+        getSelection(e.target.value)
     }
 
     return (
         <div className="form-group">
             <select value={val} onChange={handleChange} className="form-control" placeholder="Type">
-                {options.map(option => {
-                    return <option value={option} key={option} >{option}</option>
+                {options.map((option, i) => {
+                    debugger
+                    console.log(option)
+                    return <option value={option} key={i} >{option}</option>
                 })}
             </select>
         </div>
--- src/Repo.js
+++ src/Repo.js
@@ -3,32 +3,46 @@ import RepoList from './RepoList';
 import Dropdown from './Dropdown';
 import './Repo.css'
 
-const BASE_URL = "https://api.github.com/users/shalabhsingh/repos"
-const options1 = ["Type", "All", "Sources", "Forks", "Archived", "Mirrors"]
-const options2 = ["Languages", "All"]
+const options1 = ["Type","All", "Source", "Forks", "Archived", "Mirror"]
+const options2 = ["Languages", "All", "Java", "CSS", "Python", "Jupyter Notebook"]
 
 export default function Repo() {
 
     const [repo, setRepo] = useState([])
+    const [txt, setTxt] = useState('')
+    const [Selection1, setSelection1] = useState('')
+    const [Selection2, setSelection2] = useState('')
 
     useEffect(() => {
-        fetch(BASE_URL)
+        fetch(`https://api.github.com/users/shalabhsingh/repos`)
             .then(res => res.json())
             .then(data => {
                 setRepo(data)
             })
     }, [])
 
+    const onChange = (q) => {
+        setTxt(q);
+    }
+
     return (
         <div className="repo">
             <div className="repo-filter">
-                <input type="search" id="search-repo" name="search-repo" className="repo-filter-search" placeholder="Find a repository…"/>
-                <Dropdown options={options1}/>
-                <Dropdown options={options2}/>
+                <input type="search" id="search-repo" value={txt} onChange={(e) => onChange(e.target.value)} className="repo-filter-search" placeholder="Find a repository…"/>
+                <Dropdown options={options1} getSelection={(q) => setSelection1(q.toLowerCase())}/>
+                <Dropdown options={options2} getSelection={(q) => setSelection2(q.toLowerCase())}/>
             </div>
-            {repo.map(repoDetails => (
-                <RepoList key={repoDetails.id} repoDetails={repoDetails}/>
-            ))}
+            {repo.filter((data)=>{
+                {/* debugger */}
+                if({txt} == null && {Selection1} == null && {Selection2} == null)
+                    return data
+                else if(data.name.toLowerCase().includes(txt.toLowerCase())){
+                    return data
+                } 
+                }).map(repoDetails => (
+                    <RepoList key={repoDetails.id} repoDetails={repoDetails}/>
+                ))
+            }
         </div>
     )
 }
